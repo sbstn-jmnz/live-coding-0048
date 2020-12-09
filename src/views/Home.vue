76cd1558e6b0bb6995c9cc70ba24ce130e051e11
sbstn-jmnz <sbstn.jmnz@yahoo.com>
2020-12-09 18:42:33 -0300

    rouer view and home with carousel component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
+<div>
+  <CarouselComponent/>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
+</div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import CarouselComponent  from '@/components/CarouselComponent'
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    CarouselComponent
   }
 }
 </script>
